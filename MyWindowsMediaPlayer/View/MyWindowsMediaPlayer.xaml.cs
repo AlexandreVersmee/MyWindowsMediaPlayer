adf03524f60bb4cae8d85d19d92f0979aea6c96b
Alexandre Versmee <versmee.alexandre@gmail.com>
2015-01-08 23:02:04 +0100

    RE BOOM DANS TA GUEULE !!!
    DTF SALOPRI !

---
+++ MyWindowsMediaPlayer/View/MyWindowsMediaPlayer.xaml.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+
+namespace MyWindowsMediaPlayer
+{
+
+    public partial class MainWindow : Window
+    {
+        private Boolean _isReplay;
+        private Boolean _isPlaying;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            MyMediaPlayer.MediaEnded += new RoutedEventHandler(MyMediaPlayerMediaEnded);
+            MyMediaPlayer.Drop += new DragEventHandler(MyMediaPlayerDrop);
+            MyMediaPlayer.MediaFailed += MyMediaPlayerMediaFailed;
+         
+            MyMediaPlayer.LoadedBehavior = MediaState.Manual;
+            MyMediaPlayer.UnloadedBehavior = MediaState.Manual;
+  
+            this._isReplay = false;
+            this._isPlaying = false;
+        }
+
+        /* Media Element Functions */
+        #region Media Element Functions
+        private void MyMediaPlayerMediaFailed(object sender, ExceptionRoutedEventArgs e)
+        {
+            System.Diagnostics.Debug.WriteLine(e);
+        }
+        private void MyMediaPlayerDrop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                string[] filePaths = (string[])(e.Data.GetData(DataFormats.FileDrop));
+                MyMediaPlayer.Source = new Uri(filePaths[0]);
+            }
+            if (Play.ToolTip == "Lire")
+            {
+                MyMediaPlayer.Play();
+                Play.ToolTip = "Suspendre";
+            }
+        }
+        private void MyMediaPlayerMediaEnded(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (this._isReplay == true)
+                {
+                    MyMediaPlayer.Position = TimeSpan.Zero;
+                    MyMediaPlayer.Play();
+                }
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine(ex);
+            }
+
+        }
+        #endregion
+
+        /* Slider Time Function */
+
+        /* Buttons Functions  */
+        #region Buttons Functions
+        private void PlayClick(object sender, RoutedEventArgs e)
+        {
+            if (!this._isPlaying)
+            {
+                MyMediaPlayer.Play();
+                this.changeImageButton(Play, "../Images/Pause.png");
+                Play.ToolTip = "Suspendre";
+                this._isPlaying = true;
+            }
+            else
+            {
+                this.changeImageButton(Play, "../Images/Play.png");
+                Play.ToolTip = "Lire";
+                this._isPlaying = false;
+                MyMediaPlayer.Pause();
+            }
+        }
+        
+        private void MuteClick(object sender, RoutedEventArgs e)
+        {
+            if (!this._isPlaying)
+            {
+                MyMediaPlayer.IsMuted = !MyMediaPlayer.IsMuted;
+                this.changeImageButton(Sound, "../Images/Mute.png");
+                Sound.ToolTip = "Muet";
+                this._isPlaying = true;
+            }
+            else 
+            {
+                MyMediaPlayer.IsMuted = !MyMediaPlayer.IsMuted;
+                this.changeImageButton(Sound, "../Images/Volume.png");
+                Sound.ToolTip = "Volume";
+                this._isPlaying = false;
+            }
+        }
+
+        private void OpenClick(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog toto = new OpenFileDialog();
+
+            toto.Multiselect = false;
+            bool? userClickedOk = toto.ShowDialog();
+
+            if (userClickedOk == true)
+            {
+                string path = toto.FileName;
+                MyMediaPlayer.Source = new Uri(path);
+            }
+            if (!this._isPlaying)
+            {
+                MyMediaPlayer.Play();
+                Play.ToolTip = "Suspendre";
+                this._isPlaying = true;
+            }
+        }
+        
+        private void StopClick(object sender, RoutedEventArgs e)
+        {
+            MyMediaPlayer.Stop();
+        }
+
+        private void ChangeMediaVolume(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            MyMediaPlayer.Volume = (double)SliderVolume.Value;
+        }
+
+        private void ReplayClick(object sender, RoutedEventArgs e)
+        {
+            if (this._isReplay == false)
+                this._isReplay = true;
+            else
+                this._isReplay = false;
+        }
+
+        private void FullScreenClick(object sender, RoutedEventArgs e)
+        { }
+
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        { }
+        #endregion
+
+        /* Tools */
+        #region Tools
+        private void changeImageButton(Button button, string path)
+        {
+            button.Content = new Image
+            {
+                Source = new BitmapImage(new Uri(path, UriKind.Relative)),
+            };
+        }
+        #endregion
+    }
+}
